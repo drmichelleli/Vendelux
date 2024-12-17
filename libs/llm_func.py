3e49824372ad2d9cca4d8d92938715cf8f684846
drmichelleli <34017464+drmichelleli@users.noreply.github.com>
2024-12-17 01:59:16 +0000

    update the prompt classifier

--- libs/llm_func.py
+++ libs/llm_func.py
@@ -7,7 +7,6 @@ import ssl
 from aiohttp import ClientSession, TCPConnector
 from functools import partial
 from tenacity import retry, stop_after_delay, wait_fixed, retry_if_exception_type
-import logging
 from datetime import datetime
 retry_on_rate_limit_error = partial(
     retry,
@@ -16,26 +15,15 @@ retry_on_rate_limit_error = partial(
     retry=retry_if_exception_type(openai.error.RateLimitError),
 )()
 
-# define logger,file handler and set formatter
-logger = logging.getLogger(__name__)
-log_file = datetime.now().strftime('%Y_%m_%d_%H_%M')+'_llm_agent.log'
-filename='./Logs/llm/'+ log_file
-file_handler = logging.FileHandler(filename)
-logging.basicConfig(
-    format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
-    level=logging.INFO,
-    handlers=[file_handler],
-)
-
 class LLMScore():
 
     def __init__(self,text,prompt_template):
-        self.gpt_model_type_4k = "YOUR_GPT_MODEL_NAME 4k"
-        self.gpt_model_type_16k = "YOUR_GPT_MODEL_NAME 16k"
-        openai.api_key = "YOUR_OPENAI key"
-        openai.api_base = "YOUR OPENAI API BASE"
-        openai.api_version = "Your OpenAI API Version"
-        openai.api_type = "Your OpenAI API Type"
+        self.gpt_model_type_4k = "gpt-35-turbo-16k"
+        self.gpt_model_type_16k = "gpt-35-turbo"
+        openai.api_key = "YOUR_API_KEY"
+        openai.api_base = "YOUR_API_BASE"
+        openai.api_version = "2023-05-15"
+        openai.api_type = "azure"
         self.text=text
         self.prompt_template=prompt_template
         self.responses=[]
@@ -96,12 +84,10 @@ class LLMScore():
 
                 await openai.aiosession.get().close()
             except Exception as e:
-                logging.exception('')
                 await openai.aiosession.get().close()
                 pass
             
         except Exception as e:
-            logging.exception('')
             await openai.aiosession.get().close()
   
             pass
@@ -120,8 +106,6 @@ class LLMScore():
     async def gather_with_concurrency(self,n, *coros):
         self.responses=[]
         semaphore = asyncio.Semaphore(n)
-        logging.info("Starting scoring")
-        logging.info("To be scored %s calls", len(self.text))
 
         async def sem_coro(coro):
             async with semaphore:
@@ -135,8 +119,5 @@ class LLMScore():
         await  self.gather_with_concurrency(100, *coros)
 
 
-
     def return_results(self):
-        logging.info("Sent to the 4k model: %s",self.counter_4k)
-        logging.info("Sent to the 16k model: %s",self.counter_16k)
         return self.responses
